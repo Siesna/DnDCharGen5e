b9b29894b85d1dd63cab294a2d0dc846867f414d
Jaime Blalock <jaime@synaptian.com>
2015-01-18 22:37:05 -0500

    Adding logo to signin page

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'devise'
 gem 'devise-bootstrap-views'
 gem 'bootstrap-sass', '~> 3.3.1'
 gem 'autoprefixer-rails', '~> 4.0.2.2'
+gem 'haml'
 
 # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
 gem 'spring',        group: :development
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
+  authenticated :user do
+    root :to => 'home#index', :as => :authenticated_root
+  end
+  root :to => redirect('/users/sign_in')
+
   devise_for :users
   # You can have the root of your site routed with "root"
   resources :characters
-  root 'characters#index'
+  # root 'welcome#index'
   # root to: 'visitors#index'
 
 end
